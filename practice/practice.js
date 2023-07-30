2ed4ae1afcb3b27f67cfe78fbfeb709d333280b0
charlieoestreicher <71988129+charlieoestreicher@users.noreply.github.com>
2023-07-30 18:27:17 -0500

    Delete practice directory

--- practice/practice.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("pdfButton").addEventListener("click", function () {
-  // Replace 'path_to_your_pdf.pdf' with the actual path to your PDF file
-  var pdfUrl = "CO_Resume.pdf";
-
-  // Open the PDF in a new tab
-  window.open(pdfUrl, "_blank");
-});
